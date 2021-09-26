d2e723ec7e42b31310d26836af770ba06939992e
grse <info@grse.ru>
2021-09-26 14:20:39 +0300

    Added tests to check correct sorting

--- src/test/java/com/example/domainshop/DomainshopApplicationTests.java
+++ src/test/java/com/example/domainshop/DomainshopApplicationTests.java
@@ -65,4 +65,85 @@ public class DomainshopApplicationTests {
                 .andExpect(jsonPath("$[2].price").value(15.99))
                 .andExpect(jsonPath("$[2].available").value(false));
     }
+
+    @Test
+    public void testOrderByPriceAsc() throws Exception {
+
+        this.mockMvc.perform(get("/domains/available?name=existing&sortingField=price&sortingOrder=asc"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].tld").value("com"))
+                .andExpect(jsonPath("$[0].price").value(8.99))
+                .andExpect(jsonPath("$[0].available").value(false))
+                .andExpect(jsonPath("$[1].tld").value("net"))
+                .andExpect(jsonPath("$[1].price").value(9.99))
+                .andExpect(jsonPath("$[1].available").value(true))
+                .andExpect(jsonPath("$[2].tld").value("club"))
+                .andExpect(jsonPath("$[2].price").value(15.99))
+                .andExpect(jsonPath("$[2].available").value(true));
+    }
+
+    @Test
+    public void testOrderByPriceDesc() throws Exception {
+
+        this.mockMvc.perform(get("/domains/available?name=existing&sortingField=price&sortingOrder=desc"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].tld").value("club"))
+                .andExpect(jsonPath("$[0].price").value(15.99))
+                .andExpect(jsonPath("$[0].available").value(true))
+                .andExpect(jsonPath("$[1].tld").value("net"))
+                .andExpect(jsonPath("$[1].price").value(9.99))
+                .andExpect(jsonPath("$[1].available").value(true))
+                .andExpect(jsonPath("$[2].tld").value("com"))
+                .andExpect(jsonPath("$[2].price").value(8.99))
+                .andExpect(jsonPath("$[2].available").value(false));
+
+    }
+
+    @Test
+    public void testOrderByTldAsc() throws Exception {
+
+        this.mockMvc.perform(get("/domains/available?name=existing&sortingField=tld&sortingOrder=asc"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].tld").value("club"))
+                .andExpect(jsonPath("$[0].price").value(15.99))
+                .andExpect(jsonPath("$[0].available").value(true))
+                .andExpect(jsonPath("$[1].tld").value("com"))
+                .andExpect(jsonPath("$[1].price").value(8.99))
+                .andExpect(jsonPath("$[1].available").value(false))
+                .andExpect(jsonPath("$[2].tld").value("net"))
+                .andExpect(jsonPath("$[2].price").value(9.99))
+                .andExpect(jsonPath("$[2].available").value(true));
+    }
+
+    @Test
+    public void testOrderByTldDesc() throws Exception {
+
+        this.mockMvc.perform(get("/domains/available?name=existing&sortingField=tld&sortingOrder=desc"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].tld").value("net"))
+                .andExpect(jsonPath("$[0].price").value(9.99))
+                .andExpect(jsonPath("$[0].available").value(true))
+                .andExpect(jsonPath("$[1].tld").value("com"))
+                .andExpect(jsonPath("$[1].price").value(8.99))
+                .andExpect(jsonPath("$[1].available").value(false))
+                .andExpect(jsonPath("$[2].tld").value("club"))
+                .andExpect(jsonPath("$[2].price").value(15.99))
+                .andExpect(jsonPath("$[2].available").value(true));
+    }
+
+    @Test
+    public void testOrderByTldNoOrder() throws Exception {
+
+        this.mockMvc.perform(get("/domains/available?name=existing&sortingField=tld"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].tld").value("club"))
+                .andExpect(jsonPath("$[0].price").value(15.99))
+                .andExpect(jsonPath("$[0].available").value(true))
+                .andExpect(jsonPath("$[1].tld").value("com"))
+                .andExpect(jsonPath("$[1].price").value(8.99))
+                .andExpect(jsonPath("$[1].available").value(false))
+                .andExpect(jsonPath("$[2].tld").value("net"))
+                .andExpect(jsonPath("$[2].price").value(9.99))
+                .andExpect(jsonPath("$[2].available").value(true));
+    }
 }
