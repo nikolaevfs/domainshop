47be2f024dc09063c76bb4cb3e0ee87ceff1a972
grse <info@grse.ru>
2021-09-26 12:46:10 +0300

    Added tests to Git

---
+++ src/test/java/com/example/domainshop/DomainshopApplicationTests.java
@@ -0,0 +1,52 @@
+package com.example.domainshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DomainshopApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testExisting() throws Exception {
+
+        this.mockMvc.perform(get("/domains/available?name=existing"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].tld").value("com"))
+                .andExpect(jsonPath("$[0].price").value(8.99))
+                .andExpect(jsonPath("$[0].available").value(false))
+                .andExpect(jsonPath("$[1].tld").value("net"))
+                .andExpect(jsonPath("$[1].price").value(9.99))
+                .andExpect(jsonPath("$[1].available").value(true))
+                .andExpect(jsonPath("$[2].tld").value("club"))
+                .andExpect(jsonPath("$[2].price").value(15.99))
+                .andExpect(jsonPath("$[2].available").value(true));
+    }
+
+    @Test
+    public void testNotExisting() throws Exception {
+
+        this.mockMvc.perform(get("/domains/available?name=new"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].tld").value("com"))
+                .andExpect(jsonPath("$[0].price").value(8.99))
+                .andExpect(jsonPath("$[0].available").value(true))
+                .andExpect(jsonPath("$[1].tld").value("net"))
+                .andExpect(jsonPath("$[1].price").value(9.99))
+                .andExpect(jsonPath("$[1].available").value(true))
+                .andExpect(jsonPath("$[2].tld").value("club"))
+                .andExpect(jsonPath("$[2].price").value(15.99))
+                .andExpect(jsonPath("$[2].available").value(true));
+    }
+}
